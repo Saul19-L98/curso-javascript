1af698cd8fb785e23c6ce50062968773dea3877d
Saúl Laínez <saul.alejandro19@gmail.com>
2020-06-11 10:37:29 -0600

    Primera función en javascript.

--- clase1.js
+++ clase1.js
@@ -1,2 +1,7 @@
-var nombre = 'Saul', apellido = 'Lainez';
+var nombre = 'Saul', apellido = 'Lainez',edad = 22;
 var nombreCompleto = `${nombre.toUpperCase()} ${apellido.toLocaleLowerCase()}`;
+
+function imprimirEdad(){
+    console.log(`${nombre} tiene ${edad} años`)
+
+}
